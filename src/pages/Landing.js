c8a83657999baf27c4092fda352a31319c9fead3
Mohamed Samy <mohamedsamy42302@gmail.com>
2026-05-07 23:23:05 +0300

    Initial commit: Setup Landing Page & Styles

---
+++ src/pages/Landing.js
@@ -0,0 +1,82 @@
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main.png";
+import styled, { keyframes } from "styled-components";
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <img src={logo} className="logo" alt="jobster logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            I'm baby celiac tofu vegan thrifted same. Baffler +1 vape nineties, biodiesel
+            deschooling soft launch quinoa green juice hyperpop four dollar toast
+            helvetica.
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+const floatAnimation = keyframes`
+  0% {
+    transform: translateY(0px);
+  }
+  50% {
+    transform: translateY(-20px);
+  }
+  100% {
+    transform: translateY(0px);
+  }
+`;
+
+const Wrapper = styled.main`
+  nav {
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display: flex;
+    align-items: center;
+  }
+
+  .page {
+    min-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    margin-top: -3rem;
+  }
+  h1 {
+    font-weight: 700;
+    span {
+      color: var(--primary-500);
+    }
+  }
+  p {
+    color: var(--grey-600);
+  }
+  .main-img {
+    display: none;
+  }
+
+  @media (min-width: 992px) {
+    .page {
+      grid-template-columns: 1fr 1fr;
+      column-gap: 3rem;
+    }
+    .main-img {
+      display: block;
+      animation: ${floatAnimation} 3s ease-in-out infinite;
+    }
+  }
+`;
+
+export default Landing;
