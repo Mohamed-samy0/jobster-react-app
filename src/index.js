91c5d7a3d6242d6ac829d76c3f1c280d35457a99
Mohamed Samy <mohamedsamy42302@gmail.com>
2026-05-13 23:14:34 +0300

    setup: configure Redux Toolkit store and user slice

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import "normalize.css";
-import React from 'react';
-import {createRoot} from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
