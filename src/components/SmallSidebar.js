879fc2751096ca501bc15ff9e542624a0ed3f62f
Mohamed Samy <mohamedsamy42302@gmail.com>
2026-05-21 15:56:11 +0300

    Small Sidebar - Setup

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -1,9 +1,25 @@
+import { FaTimes } from "react-icons/fa";
 import Wrapper from "../assets/wrappers/SmallSidebar";
+import Logo from "./Logo";
 
 function SmallSidebar() {
   return (
     <Wrapper>
-      <h2>SmallSidebar</h2>
+      <div className="sidebar-container show-sidebar">
+        <div className="content">
+          <button
+            type="button"
+            className="close-btn"
+            onClick={() => console.log("toggle")}
+          >
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">nav links</div>
+        </div>
+      </div>
     </Wrapper>
   );
 }
