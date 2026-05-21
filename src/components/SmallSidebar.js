5dd78aecb0206f911ce43f1ea01206f18156aeea
Mohamed Samy <mohamedsamy42302@gmail.com>
2026-05-21 16:02:42 +0300

    mall Sidebar - Toggle

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -1,17 +1,22 @@
 import { FaTimes } from "react-icons/fa";
 import Wrapper from "../assets/wrappers/SmallSidebar";
 import Logo from "./Logo";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleSidebar } from "../features/user/userSlice";
 
 function SmallSidebar() {
+  const { isSidebarOpen } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const toggle = () => {
+    dispatch(toggleSidebar());
+  };
   return (
     <Wrapper>
-      <div className="sidebar-container show-sidebar">
+      <div
+        className={isSidebarOpen ? "sidebar-container show-sidebar" : "sidebar-container"}
+      >
         <div className="content">
-          <button
-            type="button"
-            className="close-btn"
-            onClick={() => console.log("toggle")}
-          >
+          <button type="button" className="close-btn" onClick={toggle}>
             <FaTimes />
           </button>
           <header>
