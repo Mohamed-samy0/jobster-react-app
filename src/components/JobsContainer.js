dfe770473ba772fca065379c5fc8c43976889172
Mohamed Samy <mohamedsamy42302@gmail.com>
2026-06-01 23:09:56 +0300

    All Jobs

--- src/components/JobsContainer.js
+++ src/components/JobsContainer.js
@@ -2,10 +2,16 @@ import { useDispatch, useSelector } from "react-redux";
 import Wrapper from "../assets/wrappers/JobsContainer";
 import Job from "./Job";
 import Loading from "./Loading";
+import { useEffect } from "react";
+import { getAllJobs } from "../features/allJobs/allJobsSlice";
 
 function JobsContainer() {
   const { jobs, isLoading } = useSelector((store) => store.allJobs);
   const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllJobs());
+  }, []);
+
   if (isLoading) {
     return <Loading center />;
   }
--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -1,11 +1,13 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import customFetch from "../../utils/axios";
+import { toast } from "react-toastify";
 
 const initialFiltersState = {
-  search: '',
-  searchStatus: 'all',
-  searchType: 'all',
-  sort: 'latest',
-  sortOptions: ['latest', 'oldest', 'a-z', 'z-a'],
+  search: "",
+  searchStatus: "all",
+  searchType: "all",
+  sort: "latest",
+  sortOptions: ["latest", "oldest", "a-z", "z-a"],
 };
 
 const initialState = {
@@ -19,9 +21,37 @@ const initialState = {
   ...initialFiltersState,
 };
 
+export const getAllJobs = createAsyncThunk("allJobs/getJobs", async (_, thunkAPI) => {
+  let url = "/jobs";
+  try {
+    const resp = await customFetch.get(url, {
+      headers: {
+        Authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
+      },
+    });
+    return resp.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.response.data.msg);
+  }
+});
+
 const allJobsSlice = createSlice({
-  name: 'allJobs',
+  name: "allJobs",
   initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAllJobs.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getAllJobs.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.jobs = payload.jobs;
+      })
+      .addCase(getAllJobs.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        toast.error(payload);
+      });
+  },
 });
 
 export default allJobsSlice.reducer;
