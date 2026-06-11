50b444636ae58c12db42c4fb7a7724997f3d197e
Mohamed Samy <mohamedsamy42302@gmail.com>
2026-06-11 14:40:38 +0300

    PageBtnContainer Structure

--- src/components/PageBtnContainer.js
+++ src/components/PageBtnContainer.js
@@ -1,5 +1,47 @@
+import { useDispatch, useSelector } from "react-redux";
+import Wrapper from "../assets/wrappers/PageBtnContainer";
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from "react-icons/hi";
+
 const PageBtnContainer = () => {
-  return <div>PageBtnContainer</div>;
+  const { numOfPages, page } = useSelector((store) => store.allJobs);
+  const dispatch = useDispatch();
+  const pages = Array.from({ length: numOfPages }, (_, index) => {
+    return index + 1;
+  });
+
+  const nextPage = () => {
+    console.log("Next page");
+  };
+  const prevPage = () => {
+    console.log("PrevNext");
+  };
+
+  return (
+    <Wrapper>
+      <button type="button" className="prev-btn" onClick={prevPage}>
+        <HiChevronDoubleLeft />
+        prev
+      </button>
+      <div className="btn-container">
+        {pages.map((pageNumber) => {
+          return (
+            <button
+              type="button"
+              key={pageNumber}
+              className={pageNumber === page ? "pageBtn active" : "pageBtn"}
+              onClick={() => console.log("Change page")}
+            >
+              {pageNumber}
+            </button>
+          );
+        })}
+      </div>
+      <button type="button" className="next-btn" onClick={nextPage}>
+        <HiChevronDoubleRight />
+        next
+      </button>
+    </Wrapper>
+  );
 };
 
 export default PageBtnContainer;
